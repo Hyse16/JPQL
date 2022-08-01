200d4a4c1f19ac49152103d0b6978e7b9bd4cfc2
hyse <gustmd4296@hanmail.net>
2022-08-01 13:47:50 +0900

    8.1(월)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -18,7 +18,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
@@ -29,21 +29,16 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', true from Member m " +
-                    "where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType",MemberType.ADMIN)
+            String query = "select nullif(m.username, '관리자') as username "+
+                    "from Member m";
+            List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (Object[] objects : result) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[0] = " + objects[1]);
-                System.out.println("objects[0] = " + objects[2]);
-
+            for (String s : resultList) {
+                System.out.println("s = " + s);
             }
 
             tx.commit();
-
 
         } catch (Exception e) {
             tx.rollback();
