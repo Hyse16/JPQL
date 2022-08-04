89b98407f71e3fd7a4aa0fa3f068302148873c2c
hyse <gustmd4296@hanmail.net>
2022-08-04 11:33:25 +0900

    8.4(목)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -26,36 +26,32 @@ public class JpaMain {
 
             Member member1= new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
-
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member1.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
+
             Member member3 = new Member();
             member3.setUsername("회원2");
+            member1.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
 
-            em.flush();
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
             em.clear();
 
-            String query = "select t From Team t ";
+            Member findMember = em.find(Member.class, member1.getId());
 
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
-                    .getResultList();
+            System.out.println("findMember.getAge() = " + findMember.getAge());
 
-            for (Team team : result) {
-                System.out.println("team.getName(),team.getMembers().size() = " + team.getName()+","+ team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("member = " + member);
-                }
-            }
             tx.commit();
 
         } catch (Exception e) {
